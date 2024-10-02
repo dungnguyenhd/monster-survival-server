aae081c1b10ad3ede5e4af424db17309e92f14a3
Nguyễn Tiến Dũng <dung1506362@nuce.edu.vn>
2024-10-02 23:06:23 +0700

    fix

--- src/main.ts
+++ src/main.ts
@@ -27,6 +27,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen();
+  await app.listen(4000);
 }
 bootstrap();
